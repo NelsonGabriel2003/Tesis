5822a88067ee7aa06350c45476a40bfe45c135d7
NelsonGabriel2003 <ngallauca@puce.edu.ec>
2026-01-09 13:18:41 -0500

    cambio de un color

--- Fronted/src/views/canje/CanjeList.jsx
+++ Fronted/src/views/canje/CanjeList.jsx
@@ -8,7 +8,7 @@ import { Star, Lock, TrendingUp } from 'lucide-react'
 const CanjeList = ({ rewards, loading, onSelectReward, canRedeem, userPoints }) => {
   if (loading) {
     return (
-      <div className="grid grid-cols-2 gap-4">
+      <div className="grid grid-cols-3 gap-4">
         {[1, 2, 3, 4].map((n) => (
           <div
             key={n}
